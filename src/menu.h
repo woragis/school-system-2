16468c0146f2c3f048446fc83b91ad012c928607
woragis <masteringthecode.woragis+github@gmail.com>
2025-05-29 02:02:08 -0300

    Separate menu from main file

---
+++ src/menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H
+#define MENU_H
+#include "bst.h"
+
+void menu(node_t **root);
+void menu_insert(node_t **root);
+void menu_remove(node_t **root); // renamed to avoid conflict
+void menu_search(node_t *root);
+void menu_delete(node_t **root);
+void menu_show(node_t *root);
+
+#endif
