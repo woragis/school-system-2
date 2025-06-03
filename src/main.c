9c6281157f8125fad9e436f2a182b67c5a12c18b
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-02 22:27:48 -0300

    Add save headers

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h> // Biblioteca padrão para funções como malloc, free, exit
 #include "bst.h"    // Header da árvore binária de busca (definições e funções de insert, search, delete, etc.)
 #include "menu.h"   // Header do menu (funções interativas do usuário)
+#include "save.h"   // Header do save (funcoes para salvar os dados)
 
 /*
  * Função principal do programa
