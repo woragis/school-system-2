ad8ddf8543bf706cfe3e42cbc97c880c8ac3e2e8
woragis <masteringthecode.woragis+github@gmail.com>
2025-06-02 22:35:41 -0300

    Separate main headers

--- src/main.c
+++ src/main.c
@@ -1,8 +1,4 @@
-#include <stdio.h>  // Biblioteca padrão de entrada/saída (printf, etc.)
-#include <stdlib.h> // Biblioteca padrão para funções como malloc, free, exit
-#include "bst.h"    // Header da árvore binária de busca (definições e funções de insert, search, delete, etc.)
-#include "menu.h"   // Header do menu (funções interativas do usuário)
-#include "save.h"   // Header do save (funcoes para salvar os dados)
+#include "main.h"
 
 /*
  * Função principal do programa
---
+++ src/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>  // Biblioteca padrão de entrada/saída (printf, etc.)
+#include <stdlib.h> // Biblioteca padrão para funções como malloc, free, exit
+#include "bst.h"    // Header da árvore binária de busca (definições e funções de insert, search, delete, etc.)
+#include "menu.h"   // Header do menu (funções interativas do usuário)
+#include "save.h"   // Header do save (funcoes para salvar os dados)
+
+#endif
